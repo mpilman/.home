bf0ffdf220ac33c652fdfa2305b9d8fcf2a0a8f6
Markus Pilman <markus.pilman@snowflake.com>
2020-04-27 17:39:14 -0700

    updated config for new doom version

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -40,11 +40,12 @@
        dired             ; making dired pretty [functional]
        ;ediff             ; comparing files in Emacs
        electric          ; smarter, keyword-based electric-indent
-       eshell            ; a consistent, cross-platform shell (WIP)
        format
        imenu             ; an imenu sidebar and searchable code index
       ;term              ; terminals in Emacs
        vc                ; version control
+       :term
+       eshell            ; a consistent, cross-platform shell (WIP)
 
        :tools
        eval              ; run code, run (also, repls)
@@ -54,7 +55,6 @@
       macos             ; MacOS-specific commands
       ;make              ; run make tasks from Emacs
        magit             ;
-       flycheck
       ;password-store    ; password manager for nerds
        ;pdf               ; pdf enhancements
       ;prodigy           ; FIXME managing external services & code builders
@@ -65,6 +65,8 @@
        ; +devdocs         ; ...on devdocs.io online
        ; +docsets)        ; ...or in Dash docsets locally
        lsp
+       :checkers
+       flycheck
 
        :lang
        assembly          ; assembly for fun or debugging
@@ -98,7 +100,7 @@
         +capture         ; org-capture in and outside of Emacs
         ;+export          ; Exporting org to whatever you want
         +present)        ; Emacs for presentations
-       perl              ; write code no one else can comprehend
+       ;perl              ; write code no one else can comprehend
       ;php               ; perl's insecure younger brother
       ;plantuml          ; diagrams for confusing people more
       ;purescript        ; javascript, but functional
